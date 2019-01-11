c19bd574262aae6a96709723e1bb54135c6b8ab3
trozio <daniel.l.adelman@gmail.com>
2019-01-10 20:53:28 -0500

    weather api call

--- assets/javascript/project.js
+++ assets/javascript/project.js
@@ -1,8 +1,8 @@
 let googleApiKey = "AIzaSyCU3cg8wL6Oip0qL_iZbpCUrdBLFbm_Lk8"
-let cityName = Atlanta
-let countryCode = US
+let cityName = "atlanta"
+let countryCode = "us"
 let weatherApiKey = "75598549dfb84653561068b1a40f42c2"
-let weatherApi = "api.openweathermap.org/data/2.5/forecast?q=" + cityName + "," + countryCode + "&APPID=" + weatherApiKey
+let weatherApi = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "," + countryCode + "&appid=" + weatherApiKey
 var config = {
     apiKey: "AIzaSyCt1tjlPv6urCLqmPuzSLoyVnGIevTPjds",
     authDomain: "project-one-64b32.firebaseapp.com",
@@ -12,3 +12,18 @@ var config = {
     messagingSenderId: "668349026732"
   };
   firebase.initializeApp(config);
+
+
+
+function weather(){
+    $.ajax({
+        url: weatherApi,
+        method: "GET"
+      }).then(function(response) {
+         console.log(response);
+         console.log((response.main.temp - 273.15) * 1.80 + 32)
+    })
+}
+
+
+  weather();
