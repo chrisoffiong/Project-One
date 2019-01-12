ee7268f1f5f67377f1904008721ab4ec3a2819bb
trozio <daniel.l.adelman@gmail.com>
2019-01-12 11:05:28 -0500

    updated on click

--- assets/javascript/project.js
+++ assets/javascript/project.js
@@ -1,8 +1,4 @@
-let googleApiKey = "AIzaSyCU3cg8wL6Oip0qL_iZbpCUrdBLFbm_Lk8"
-let cityName = "atlanta"
-let countryCode = "us"
-let weatherApiKey = "75598549dfb84653561068b1a40f42c2"
-let weatherApi = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "," + countryCode + "&appid=" + weatherApiKey
+
 var config = {
   apiKey: "AIzaSyCt1tjlPv6urCLqmPuzSLoyVnGIevTPjds",
   authDomain: "project-one-64b32.firebaseapp.com",
@@ -13,20 +9,35 @@ var config = {
 };
 firebase.initializeApp(config);
 
+$("#submit").on("click", function(event){
+    event.preventDefault();
+    let googleApiKey = "AIzaSyCU3cg8wL6Oip0qL_iZbpCUrdBLFbm_Lk8"
+    let cityName = $("#cityInput").val();
+    let countryCode = "us"
+    let weatherApiKey = "75598549dfb84653561068b1a40f42c2"
+    let weatherApi = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "," + countryCode + "&appid=" + weatherApiKey
+    let state = $("#stateInput").val();
+    let areaCode = $("#areaCodeInput").val();
+    console.log(cityName);
+    console.log(state);
+    console.log(areaCode);
+
+      $.ajax({
+        url: weatherApi,
+        method: "GET"
+      }).then(function (response) {
+        console.log(response);
+        let latitude = response.coord.lat;
+        let longitude = response.coord.lon;
+        console.log("Latitude: " + latitude + " Longitude: " + longitude);
+        console.log((response.main.temp - 273.15) * 1.80 + 32);
+      })
+
+})
 
 
-function weather() {
-  $.ajax({
-    url: weatherApi,
-    method: "GET"
-  }).then(function (response) {
-    console.log(response);
-    console.log((response.main.temp - 273.15) * 1.80 + 32)
-  })
-}
 
 
-weather();
 
 function initMap() {
 
@@ -57,14 +68,3 @@ var locations = [{
     }]
 
     initMap();
-
-
-$("#submit").on("click", function(event){
-    event.preventDefault();
-    let city = $("#cityInput").val();
-    let state = $("#stateInput").val();
-    let areaCode = $("#areaCodeInput").val();
-    console.log(city);
-    console.log(state);
-    console.log(areaCode);
-})
