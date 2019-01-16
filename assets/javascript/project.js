9418c972dc105f6eff865bc08aa63691b13ae148
trozio <daniel.l.adelman@gmail.com>
2019-01-15 19:55:09 -0500

    maps experiment

--- assets/javascript/project.js
+++ assets/javascript/project.js
@@ -48,68 +48,86 @@ $("#submit").on("click", function(event){
       var map;
 
 
-      function initialize() {
-        let pyrmont;
-        if ($("#atlanta").val() == "atlanta") {
-          pyrmont = new google.maps.LatLng(33.7756,-84.3963);
-        }
-        else if ($("#boston").val() == "boston") {
-           pyrmont = new google.maps.LatLng(42.3601,-71.0589)
-        }
-        else if ($("#newyork").val() == "new york") {
-           pyrmont = new google.maps.LatLng(40.7831,-73.9712)
-        }
-        else if ($("#portland").val() == "portland") {
-           pyrmont = new google.maps.LatLng(45.5122,-122.6587)
-        }
-        else if ($("#seattle").val() == "seattle") {
-           pyrmont = new google.maps.LatLng(47.6062, -122.3321)
-        }
 
-        var request;
-        map = new google.maps.Map(document.getElementById('mapDisplay'), {
-            center: pyrmont,
-            zoom: 13
-          });
-      if ($("#shops").val() == "shops") {
-        var request = {
-          location: pyrmont,
-          radius: '5000',
-          type: ['bicycle_store']
-        }
-      }
-      else if ($("#attractions").val() == "attractions") {
-        var request = {
-          location: pyrmont,
-          radius: '5000',
-          type: ['stadium','aquarium','movie_theater','bar']
-      }
-    }
-       ;
+          // This example adds a search box to a map, using the Google Place Autocomplete
+          // feature. People can enter geographical searches. The search box will return a
+          // pick list containing a mix of places and predicted search terms.
 
-        var service = new google.maps.places.PlacesService(map);
-        service.nearbySearch(request, callback);
-      }
+          // This example requires the Places library. Include the libraries=places
+          // parameter when you first load the API. For example:
+          // <script src="https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&libraries=places">
 
+          function initialize() {
+            var map = new google.maps.Map(document.getElementById('map'), {
+              center: {lat: -33.8688, lng: 151.2195},
+              zoom: 13,
+              mapTypeId: 'roadmap'
+            });
 
-      function callback(results, status) {
-        if (status == google.maps.places.PlacesServiceStatus.OK) {
-          for (var i = 0; i < results.length; i++) {
-            createMarker(results[i]);
+            // Create the search box and link it to the UI element.
+            var input = $(".icons").val();
+            var searchBox = new google.maps.places.SearchBox(input);
+            map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+
+            // Bias the SearchBox results towards current map's viewport.
+            map.addListener('bounds_changed', function() {
+              searchBox.setBounds(map.getBounds());
+            });
+
+            var markers = [];
+            // Listen for the event fired when the user selects a prediction and retrieve
+            // more details for that place.
+            searchBox.addListener('places_changed', function() {
+              var places = input.getPlaces();
+
+              if (places.length == 0) {
+                return;
+              }
+
+              // Clear out the old markers.
+              markers.forEach(function(marker) {
+                marker.setMap(null);
+              });
+              markers = [];
+
+              // For each place, get the icon, name and location.
+              var bounds = new google.maps.LatLngBounds();
+              places.forEach(function(place) {
+                if (!place.geometry) {
+                  console.log("Returned place contains no geometry");
+                  return;
+                }
+                var icon = {
+                  url: place.icon,
+                  size: new google.maps.Size(71, 71),
+                  origin: new google.maps.Point(0, 0),
+                  anchor: new google.maps.Point(17, 34),
+                  scaledSize: new google.maps.Size(25, 25)
+                };
+
+                // Create a marker for each place.
+                markers.push(new google.maps.Marker({
+                  map: map,
+                  icon: icon,
+                  title: place.name,
+                  position: place.geometry.location
+                }));
+
+                if (place.geometry.viewport) {
+                  // Only geocodes have viewport.
+                  bounds.union(place.geometry.viewport);
+                } else {
+                  bounds.extend(place.geometry.location);
+                }
+              });
+              map.fitBounds(bounds);
+            });
           }
-        }
+          initialize();
+          google.maps.event.addDomListener(window, 'load', initialize);
+      })
 
-      }
-      function createMarker(place) {
-        var placeLoc = place.geometry.location;
-        var marker = new google.maps.Marker({
-          map: map,
-          position: place.geometry.location
-        })
-      }
-      initialize()
-      google.maps.event.addDomListener(window, 'load', initialize)
-})
+
 
 $(document).ready(function(){
   $('select').formSelect();
